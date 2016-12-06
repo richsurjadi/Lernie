afc155c5cf942e636ea620711ab4d3a495d3ee5a
Richard Surjadi <richsurjadi@gmail.com>
2016-12-06 14:05:56 +1100

    Added Subjects Page ViewController with label and tableview

---
+++ SubjectsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SubjectsViewController.swift
+//  Lernie
+//
+//  Created by Richard Surjadi on 12/6/16.
+//  Copyright © 2016 Richard Surjadi. All rights reserved.
+//
+
+import UIKit
+
+class SubjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   }
